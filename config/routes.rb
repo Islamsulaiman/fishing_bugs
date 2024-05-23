ca7c41f3f678320c0ebb7740947a653fefec53c2
dabour1 <Aldabor63@Yahoo.Com>
2024-05-23 19:24:41 +0300

    add index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts
+  # resources :posts
 
   get "posts", to: "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
