2b29104477be5e3033833e4ab96caa65015bdd86
mennazm <nonahoda2000@gmail.com>
2024-05-23 16:09:05 +0300

    Correct Route Defination (indix --> index)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
 
-  get "posts", to: "posts#indix"
+  #get "posts", to: "posts#indix"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  get "posts", to: "posts#index"
+
 end
